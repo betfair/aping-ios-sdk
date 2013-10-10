16e9b9cfbc76ff4867d3097e9b41e7f3f99fa494
seanoshea <oshea.ie@gmail.com>
2013-10-10 08:47:10 +0100

    Updating the license.

--- APINGExampleApplication/APINGExampleApplication/APINGExampleApplication/APINGExampleAppDelegate.h
+++ APINGExampleApplication/APINGExampleApplication/APINGExampleApplication/APINGExampleAppDelegate.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2013, Betfair
+// Copyright (c) 2013, The Sporting Exchange Limited
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
@@ -10,15 +10,15 @@
 // documentation and/or other materials provided with the distribution.
 // 3. All advertising materials mentioning features or use of this software
 // must display the following acknowledgement:
-// This product includes software developed by the <organization>.
-// 4. Neither the name of the <organization> nor the
+// This product includes software developed by the The Sporting Exchange Limited.
+// 4. Neither the name of the The Sporting Exchange Limited nor the
 // names of its contributors may be used to endorse or promote products
 // derived from this software without specific prior written permission.
 //
-// THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+// THIS SOFTWARE IS PROVIDED BY THE SPORTING EXCHANGE LIMITED ''AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+// DISCLAIMED. IN NO EVENT SHALL THE SPORTING EXCHANGE LIMITED BE LIABLE FOR ANY
 // DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 // (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 // LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
